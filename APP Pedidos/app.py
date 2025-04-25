3c676c85e9281a722a984462e4139136b744f7da
4ndrig0 <andrigo2006ju@gmail.com>
2025-04-25 14:32:24 -0300

    Deu por hoje

---
+++ APP Pedidos/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+
+def home():
+    return render_template('index.html')
+
+#def home2():
+#    return render_template('index2.html')
+
+if __name__ == '__main__': 
+    app.run(debug=True)
